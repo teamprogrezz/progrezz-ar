761dee84ecee4bad8f3545caa6079c21de824f97
Cristo González Rodríguez <cristogr.93@gmail.com>
2015-10-05 14:58:49 +0100

    Actualizando alertas

--- js/Viewer.js
+++ js/Viewer.js
@@ -433,7 +433,7 @@ ARProgrezz.Viewer = function () {
     this.addObject = function (options) {
 
         var object;
-alert("Función:" + JSON.stringify(options));
+
         // Creación del objeto dependiendo del tipo
         switch (options.type) {
         case 'basic':
